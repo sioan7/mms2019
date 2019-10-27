ca4bbb1996f9a15dd47c34ce9c0f3788f7a0a65b
Stănilă Ioan <stanila.ionutsss@gmail.com>
2019-10-27 14:13:22 +0100

    code style for main activity

--- app/src/main/java/com/example/worldcanvas/MainActivity.kt
+++ app/src/main/java/com/example/worldcanvas/MainActivity.kt
@@ -21,9 +21,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         providers = Arrays.asList<AuthUI.IdpConfig>(
-               AuthUI.IdpConfig.EmailBuilder().build(),
-               AuthUI.IdpConfig.GoogleBuilder().build(),
-               AuthUI.IdpConfig.PhoneBuilder().build()
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build()
         )
 
         showSignInOptions()
@@ -31,38 +31,38 @@ class MainActivity : AppCompatActivity() {
         //Event
         sign_out.setOnClickListener {
             AuthUI.getInstance().signOut(this@MainActivity)
-                    .addOnCompleteListener {
-                        sign_out.isEnabled = false
-                        showSignInOptions()
-                    }
-                    .addOnFailureListener {
-                        e -> Toast.makeText(this@MainActivity,""+e.message,Toast.LENGTH_SHORT).show()
-                    }
+                .addOnCompleteListener {
+                    sign_out.isEnabled = false
+                    showSignInOptions()
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+                }
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == MY_REQUEST_CODE){
+        if (requestCode == MY_REQUEST_CODE) {
             val response = IdpResponse.fromResultIntent(data)
-            if(resultCode == Activity.RESULT_OK)
-            {
+            if (resultCode == Activity.RESULT_OK) {
                 val user = FirebaseAuth.getInstance().currentUser
-                Toast.makeText(this,""+user!!.email, Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, user!!.email, Toast.LENGTH_SHORT).show()
 
                 sign_out.isEnabled = true
-            }
-            else{
-                Toast.makeText(this,""+response!!.error!!.message, Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, response!!.error!!.message, Toast.LENGTH_SHORT).show()
 
             }
         }
     }
 
     private fun showSignInOptions() {
-        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder()
                 .setAvailableProviders(providers)
                 .setTheme(R.style.MyTheme)
-                .build(), MY_REQUEST_CODE)
+                .build(), MY_REQUEST_CODE
+        )
     }
 }
